cbbf0ec0053ebbc5182e157ca43ee6dd373cc77a
Eldos106 <toktosunoveldos99@gmail.com>
2025-04-14 17:57:39 +0600

    Test-1

---
+++ main.js
@@ -0,0 +1,50 @@
+/// 1
+
+const containsOnlyDigits = (str) => {
+    const regExp = /^\d+$/;
+    return regExp.test(str);
+}
+
+console.log(containsOnlyDigits("12345")); // Выведет true
+console.log(containsOnlyDigits("12a45")); // Выведет false
+
+/// 2
+
+setInterval(() => {
+    console.log("Прошла секунда");
+}, 1000);
+
+/// 3
+
+const count = () => {
+    let i = 1;
+    const interval = setInterval(() => {
+        console.log(i);
+        i++;
+        if (i > 10) {
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+
+count();
+
+/// 4
+
+const box = document.getElementById('box');
+box.addEventListener('click', () => {
+    box.classList.toggle('bg-color');
+});
+
+/// 5
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', './test.json');
+xhr.setRequestHeader('Content-type', 'application/json')
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        const data = JSON.parse(xhr.response);
+        console.log(data);
+    }
+};
+xhr.send();
